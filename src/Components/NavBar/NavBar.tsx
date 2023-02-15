ef6caab45eb3ef49bc4ea8d328d5f2beec027d76
Scott Vore <svore@hawaii.edu>
2023-02-15 09:41:45 -1000

    updating naming and flow

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import {AppBar, Button} from "@mui/material";
+
+const pages = ["Home", "Products", "FAQs"];
+const settings = ["Mode"];
+
+function NavBar() {
+    return (
+        <div id="nav">
+            <AppBar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default NavBar
---
+++ src/Pages/Landing/Landing.tsx
@@ -0,0 +1,26 @@
+import logo from "../../Assets/resources/logo.svg";
+import React from "react";
+import './Assets/styles/css/App.css';
+
+function Landing() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Landing;
